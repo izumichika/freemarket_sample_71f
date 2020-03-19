40061e5843738a77cce8b29410895348ed1f36a8
izumichika <chikara0707@outlook.jp>
2020-03-19 14:49:17 +0900

    create users registration view
    ターミナルからデバイス専用のユーザー登録画面を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
-  resource :items, only: :index
+  # root to: 'items#index'
+  # resource :items, only: :index
+
+  unauthenticated do
+    as :user do
+      root :to => 'devise/registrations#new'
+    end
+  end
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
